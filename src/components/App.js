49f630770fbf21a029565d0a4f11d8e47983c4e6
SeobisBack <seobisback@gmail.com>
2021-11-29 01:47:03 +0900

    Connect FireBase

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react';
-import AppRouter from './Router';
+import AppRouter from 'components/Router';
+import { authService } from '../fbase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn}/>
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn}/>
+    <footer>&copy; {new Date().getFullYear()} Switter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,53 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { authService } from '../fbase';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const onChange = (event) =>{
+        const {target: {name, value}} = event;
+        if (name === "email"){
+            setEmail(value);
+        } else if(name === "password"){
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            let data;
+            if(newAccount) {
+                // create account
+                data = await createUserWithEmailAndPassword(authService,
+                    email, password
+                );
+            } else {
+                // Log In
+                data = await signInWithEmailAndPassword(authService,
+                    email, password
+                );
+            }
+            console.log(data);
+        }catch(error){
+            console.log(error);
+        }
+    };
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input name="email" type="email" placeholder='E-Mail' required value={email} onChange={onChange}/>
+            <input name="password" type="password" placeholder='Password' required value={password} onChange={onChange}/>
+            <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+    </div>
+    );
+};
 export default Auth;

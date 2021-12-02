8db64ccb7c8264bd725777303d4ab77465b8f204
SeobisBack <seobisback@gmail.com>
2021-12-03 00:57:08 +0900

    Make File Upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from 'react';
 const Home = ({ userObj }) => {
     const [switt, setSwitt] = useState("");
     const [switts, setSwitts] = useState([]);
+    const [attachment, setAttachment] = useState(); // file url 관리
     useEffect(() => {
         const q = query(collection(dbService, "switts"), orderBy("createAt", "desc"));
         onSnapshot(q, (snapshot) => {
@@ -34,13 +35,30 @@ const Home = ({ userObj }) => {
         const {target:{value},} = event;
         setSwitt(value);
     };
-    console.log(switts);
+    const onFileChange = (event) => {
+        const {target:{files}, } = event;
+        const theFile = files[0];
+        const reader = new FileReader(); // FileReader API
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: { result }} = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={switt} onChange={onChange} type="text" placeholder="What's on your mind ? " maxLength={120}/>
+                <input type='file' accept='image/*' onChange={onFileChange}/>
                 <input type="submit" value = "Switt"/>
-            </form>
+                {attachment && (
+                    <div>
+                        <img src={attachment} width='50px' height='50px'/>
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )} 
+            </form> 
             <div>
                 {switts.map((switt) => (
                     <Switt key={switt.id} swittObj={switt} isOwner={switt.creatorId === userObj.uid}/>
